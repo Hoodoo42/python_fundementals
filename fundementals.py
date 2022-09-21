a2dca6c476bff40798780b98129cc341728fab75
Hoodoo42 <create@hoodooverse.ca>
2022-09-20 20:47:58 -0600

    mixing loops and if else inside the functions

--- fundementals.py
+++ fundementals.py
@@ -1,4 +1,7 @@
-age =  0
+from concurrent.futures.process import _check_system_limits
+
+
+age = 0
 name = 'Charlie'
 bo = False
 
@@ -7,23 +10,23 @@ print(name)
 print(bo)
 
 
-if(age > 10):
+if (age > 10):
     print('That is larger than 10')
 else:
     print('That is not larger than 10')
 
 
-if(age < 0 and bo == True):
+if (age < 0 and bo == True):
     print('Negative and True')
-elif(age > 0 and bo == False):
+elif (age > 0 and bo == False):
     print('Positive and False')
-elif(age > 100 or bo == True):
+elif (age > 100 or bo == True):
     print('Large or True')
 else:
     print("I don't know")
 
 animals = ['bear', 'lion', 'tiger', 'owl']
-numbers = [1,2,3,4]
+numbers = [1, 2, 3, 4]
 
 for animal in animals:
     print(animal)
@@ -34,11 +37,13 @@ for number in numbers:
 
 def static_greeting():
     print('Hello Colleen')
+
+
 static_greeting()
 
-def dynamic_greeting(real_name):
-    print('Hello',real_name)
 
+def dynamic_greeting(real_name):
+    print('Hello', real_name)
 
 
 real_name = 'Bob'
@@ -49,3 +54,20 @@ dynamic_greeting(real_name)
 
 real_name = 'Rick'
 dynamic_greeting(real_name)
+
+
+def find_treasure(strings):
+    for buried in strings:
+        if (buried == 'treasure'):
+            print('Treasure Found!')
+            return True
+
+    print('No treasure')
+    return False
+
+
+chest_one = ['sand', 'water', 'treasure']
+find_treasure(chest_one)
+
+chest_two = ['tree', 'cave', 'sky']
+find_treasure(chest_two)
